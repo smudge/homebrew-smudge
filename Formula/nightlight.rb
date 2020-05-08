db50fd644d1d6fd332e3494f35f43d1c1596f783
smudge <nathan@ngriffith.com>
2020-05-07 23:41:06 -0400

    nightlight v0.1.0 => v0.1.1

--- Formula/nightlight.rb
+++ Formula/nightlight.rb
@@ -1,9 +1,9 @@
 class Nightlight < Formula
   desc "A CLI for configuring 'Night Shift' on macOS 🌕🌖🌗🌘🌑"
   homepage "https://github.com/smudge/nightlight"
-  url "https://github.com/smudge/nightlight/archive/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "ee4ffbafbaabc9be7376875f84ec16d62dc07223a878559dd5361c36bf063f1e"
+  url "https://github.com/smudge/nightlight/archive/v0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "8d99a0dec5a11d5b73a9e82a24f4b51a8d18719d6ab85428b66bc18b3219063b"
 
   depends_on "rust" => :build
 
