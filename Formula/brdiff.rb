6a4581895280c181eafc9cbd4dbe3f231c94054d
smudge <nathan@ngriffith.com>
2020-12-05 21:07:30 -0500

    brdiff 0.2.0

--- Formula/brdiff.rb
+++ Formula/brdiff.rb
@@ -1,9 +1,9 @@
 class Brdiff < Formula
   desc "A command-line tool for subtracting images to generate a new blue-red difference image"
   homepage "https://github.com/smudge/brdiff"
-  url "https://github.com/smudge/brdiff/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "1c57fb51f832ca7374c1ee73fb767f1079319f05593b25f0701ac84df7027693"
+  url "https://github.com/smudge/brdiff/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "990bd4149e311190b69e442f413b910fc4c3ecf1d25572e4a32501e0006e5966"
 
   depends_on "rust" => :build
 
